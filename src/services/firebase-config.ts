1099103f1e61e47713351061145779c64a42ce93
nichol6s <nicholas.jake15@gmail.com>
2024-06-05 09:01:48 -0300

    feat: Update tab icons and colors in TabLayout
    chore: Add firebase configuration file
    
    The recent code changes include updating the tab icons and colors in the TabLayout component. The Ionicons import has been replaced with MaterialIcons for the "home" tab, and the MaterialCommunityIcons import has been replaced with MaterialIcons for the "ocean" tab. The inactive and active tint colors have also been updated.
    
    Additionally, a new file `firebase-config.ts` has been added to the `services` directory. This file contains the Firebase configuration and initializes the Firebase app and Firestore.

---
+++ src/services/firebase-config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWzXZW_EW60llbpHVRSOoyFkFYpa_Ma-U",
+  authDomain: "bluesense-database.firebaseapp.com",
+  projectId: "bluesense-database",
+  storageBucket: "bluesense-database.appspot.com",
+  messagingSenderId: "290498466335",
+  appId: "1:290498466335:web:f009d1aa8e1514ca69bb2a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
