173bd4403ffe3b746c0e0c5e31d830a67bf36f7d
nichol6s <nicholas.jake15@gmail.com>
2024-06-06 00:02:45 -0300

    chore: Update FontAwesome6 and FontAwesome5 icon sizes in IncidentReport component

--- src/components/IncidentReport.tsx
+++ src/components/IncidentReport.tsx
@@ -10,26 +10,26 @@ export default function IncidentReport() {
             <Text className='text-lg'>Sujeira forte e poluente dentro da rota</Text>
 
             <View className='flex-row items-center gap-1 mt-2'>
-                <FontAwesome6 name="location-dot" color="gray" size={8} />
-                <Text className='text-xs'>Rota do Pacífico</Text>
+                <FontAwesome6 name="location-dot" color="gray" size={10} />
+                <Text className='text-sm'>Rota do Pacífico</Text>
 
-                <FontAwesome5 name="ship" color="gray" size={8} />
-                <Text className='text-xs'>UU316KL</Text>
+                <FontAwesome5 name="ship" color="gray" size={10} />
+                <Text className='text-sm'>UU316KL</Text>
 
                 <View className='flex-row ml-16 gap-8'>
                     <Pressable className='active:opacity-70'>
-                        <MaterialIcons name="edit" color="green" size={30} />
+                        <MaterialIcons name="edit" color="green" size={28} />
                     </Pressable>
 
                     <Pressable className='active:opacity-50'>
-                        <Feather name="trash" color="red" size={30} />
+                        <Feather name="trash" color="red" size={28} />
                     </Pressable>
                 </View>
             </View>
 
             <View className='flex-row items-center gap-1'>
-                <FontAwesome name="circle" color="blue" size={8} />
-                <Text className='text-xs'>Em andamento</Text>
+                <FontAwesome name="circle" color="blue" size={10} />
+                <Text className='text-sm'>Em andamento</Text>
             </View>
 
 
